40ce297cb00d4ed10b376f1051926ebaf4cc14ce
venkey27 <venkey271200@gmail.com>
2026-07-07 19:29:09 -0400

    terraform

--- 40-databases/r53.tf
+++ 40-databases/r53.tf
@@ -6,8 +6,8 @@ resource "aws_route53_record" "mongodb" {
   type    = "A"
   ttl     = 1
   records = [aws_instance.mongodb.private_ip]
-  allow_overwrite = true
-}
+  allow_overwrite = true # I know this record might already exist in the AWS console. If it does, do not crash. Just smash right through it and 
+}                                   #overwrite its IP address with my new instance's IP address anyway."
 
 
 resource "aws_route53_record" "redis" {
