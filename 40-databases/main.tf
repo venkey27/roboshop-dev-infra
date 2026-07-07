703320fc903ec87def9bf24183ca18394eb7de96
venkey27 <venkey271200@gmail.com>
2026-07-07 13:35:21 -0400

    special variables

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -37,7 +37,7 @@ resource "terraform_data" "mongodb" {
   provisioner "remote-exec" {
     inline = [
       "chmod +x /tmp/bootstrap.sh",
-      "sudo sh /tmp/bootstrap.sh mongodb ${var.environment}"
+      "sudo sh /tmp/bootstrap.sh mongodb ${var.environment}" 
     ]
   }
 }
