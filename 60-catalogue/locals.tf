773f2fbf3ebd25926c2e56a38c0b2561c37a1990
venkey27 <venkey271200@gmail.com>
2026-07-10 14:07:03 -0400

    terraform

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -1,7 +1,7 @@
 locals {
     ami_id = data.aws_ami.joindevops.id
     common_name = "${var.project}-${var.environment}"
-    catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id
+    catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
     private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]  #we just prefer us-east-1  # split create List(String)
     common_tags = {
         Project = "${var.project}"
