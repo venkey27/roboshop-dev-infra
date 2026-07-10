56003c262773ad73f563a55db682cae33b84b88d
venkey27 <venkey271200@gmail.com>
2026-07-10 14:04:59 -0400

    terraform

--- 60-catalogue/data.tf
+++ 60-catalogue/data.tf
@@ -2,7 +2,7 @@
 
 
 data "aws_ssm_parameter" "catalogue_sg_id" {
-    name = "/${var.project}/${var.environment}/catalogue_alb_sg_id"
+    name = "/${var.project}/${var.environment}/catalogue_sg_id"
 }
 
 data "aws_ssm_parameter" "private_subnet_ids" {
