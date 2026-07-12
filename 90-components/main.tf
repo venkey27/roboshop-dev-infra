33c9406f617f4eb1512f38f5434a04c04305b007
venkey27 <venkey271200@gmail.com>
2026-07-12 19:30:59 -0400

    terraform

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -4,4 +4,5 @@ module "components"{
     environment = var.environment
     component = each.key                      # key = catalogue
     app_version = each.value.app_version      # each.value = {app_version = "v3"} # each.value.app_version = "v3"
+    rule_priority = each.value.rule_priority
 } 
