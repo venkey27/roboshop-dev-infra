80a9dc6bafaa5fa390a586a0d37dfb9381c50cf4
venkey27 <venkey271200@gmail.com>
2026-07-12 16:41:23 -0400

    terraform

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,6 +1,6 @@
 module "components"{
     for_each = var.components
-    source = "../../terraform-roboshop-component"
+    source = "git::https://github.com/venkey27/terraform-roboshop-component.git?ref=main"
     environment = var.environment
     component = each.key                      # key = catalogue
     app_version = each.value.app_version      # each.value = {app_version = "v3"} # each.value.app_version = "v3"
