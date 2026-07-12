efad297d071ed93bc13b230b6624f7f5be88a333
venkey27 <venkey271200@gmail.com>
2026-07-12 18:08:02 -0400

    terraform

--- 90-components/variables.tf
+++ 90-components/variables.tf
@@ -14,11 +14,32 @@ variable "domain_name" {
     default = "exptrack.shop"
 }
 
+
 variable "components" {
     default = {
-        catalogue = {               # <-- This is the KEY (each.key)
-           # rule_priority = 10
-           app_version = "v3"       # # <-- # each.value = {app_version = "v3"}   # each.value.app_version = "v3"
+        catalogue = {                      # <-- This is the KEY (each.key)
+            rule_priority = 10
+            app_version = "v3"             # <-- # each.value = {app_version = "v3"}   # each.value.app_version = "v3"
         }
+        user = {
+            rule_priority = 20
+            app_version = "v3"
+        }
+        cart = {
+            rule_priority = 30
+            app_version = "v3"
+        }
+        shipping = {
+            rule_priority = 40
+            app_version = "v3"
+        }
+        payment = {
+            rule_priority = 50
+            app_version = "v3"
+        }
+        # frontend = {
+        #     rule_priority = 10
+        #     app_version = "v3"
+        # }
     }
 }
