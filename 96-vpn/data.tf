1d782149ce844c1e8aa32e983b72a07c761487ea
venkey27 <venkey271200@gmail.com>
2026-07-16 19:55:15 -0400

    terraform creation

--- 96-vpn/data.tf
+++ 96-vpn/data.tf
@@ -1,11 +1,32 @@
-data "aws_cloudfront_cache_policy" "cachingOptmized" {
-  name = "Managed-CachingOptimized"
+data "aws_ssm_parameter" "vpn_sg_id" {
+    name = "/${var.project}/${var.environment}/vpn_sg_id"
 }
 
-data "aws_cloudfront_cache_policy" "cachingDisabled" {
-  name = "Managed-CachingDisabled"
+data "aws_ssm_parameter" "public_subnet_ids" {
+    name = "/${var.project}/${var.environment}/public_subnet_ids"
 }
 
-data "aws_ssm_parameter" "certificate_arn" {
-    name = "/${var.project}/${var.environment}/certificate_arn"
+data "aws_ami" "openvpn" {
+  most_recent      = true
+  owners           = ["679593333241"]
+
+  filter {
+    name   = "name"
+    values = ["OpenVPN Access Server Community Image-8fbe3379-*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
 }
